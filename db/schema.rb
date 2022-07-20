795d7193128a316f336515f3ba8d6e4d8b06d885
kiizerd <ajsewell13@gmail.com>
2022-07-20 15:30:43 -0500

    Generated Task model and added routes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_13_235822) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_19_173436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,4 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_13_235822) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.string "status"
+    t.boolean "complete"
+    t.bigint "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_tasks_on_project_id"
+  end
+
+  add_foreign_key "tasks", "projects"
 end
