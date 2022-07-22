5a5dd35542b5974533df33871fc8778518ed4a3e
kiizerd <ajsewell13@gmail.com>
2022-07-22 13:20:53 -0500

    Generated Task model and associated with Project model.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,16 @@
 class Task < ApplicationRecord
+  include Completable
+
   belongs_to :project
 
-  validates :name, presence: true  
+  validates :name, presence: true
+
+  before_validation :ensure_status_has_a_value
+
+  private
+    def ensure_status_has_a_value
+      if status.nil?
+        self.status = 'standby'
+      end
+    end
 end
