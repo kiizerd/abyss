8bff78a0f723b1af9bfe0b1b120fdc00ca345d10
kiizerd <ajsewell13@gmail.com>
2022-08-04 11:40:36 -0500

    Simplified database schema.

--- db/migrate/20220719173436_create_tasks.rb
+++ db/migrate/20220719173436_create_tasks.rb
@@ -2,8 +2,7 @@ class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
       t.string :name
-      t.string :status
-      t.boolean :complete
+      t.integer :status
       t.references :project, null: false, foreign_key: true
 
       t.timestamps
