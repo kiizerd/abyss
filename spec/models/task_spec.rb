5de26728cbb4650c0ea5c636e66cb26bdb67756b
kiizerd <ajsewell13@gmail.com>
2022-08-05 16:46:55 -0500

    Removed unnecessary tests for Task model.

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,27 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  subject do
-    project = Project.create(name: 'task_project',
-                             description: 'Project for testing.')
-    described_class.new(
-      name: 'test task',
-      project_id: project.id
-    )
-  end
-
-  describe "associations" do
-    it { should belong_to(:project) }
-  end
-
   describe "validations" do
-    it "is valid with valid attributes" do
-      expect(subject).to be_valid
-    end
-
-    it "is not valid without name" do
-      subject.name = nil
-      expect(subject).to_not be_valid
-    end
+    it { is_expected.to validate_presence_of(:name) }
   end
 end
