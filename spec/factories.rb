a3c663ca2c9782eccd5100d44a6210e1e4ebf234
kiizerd <ajsewell13@gmail.com>
2022-08-04 20:00:03 -0500

    Wrote first test suite.

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :project do
+    name { "Factory-made Project" }
+    description { "A testing object made with FactoryBot." }
+  end
+end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    project = Project.create(name: 'task_project',
+                             description: 'Project for testing.')
+    described_class.new(
+      name: 'test task',
+      project_id: project.id
+    )
+  end
+
+  describe "associations" do
+    it { should belong_to(:project) }
+  end
+
+  describe "validations" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without name" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
